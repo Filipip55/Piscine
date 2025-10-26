daaaa8284667f9c7dafc95a8eccf7e4725f88cf3
Filip Coman <filipcoman@gmail.it>
2025-10-26 13:52:35 +0100

    fatto tutto

--- main.c
+++ main.c
@@ -13,29 +13,45 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int	check_input_for_error(int n_input, char **input)
+int	check_input(int n_input, char *input)
 {
 	int i;
+	int	nbr_count;
+	int	spc_count;
 
-	i = 0;
 	if (n_input != 2)
+		return (0);
+	spc_count = 0;
+	nbr_count = 0;
+	i = 0;
+	while (input[i])
 	{
-		return (1);
-	}
-	while (input[1][i])
-	{
-		if (!((input[1][i] >= '1' && input[1][i] <= '4') || input[1][i] == ' '))
-			return (1);
-		i++;
+		if (spc_count > 1)
+			return (0);
+		if (nbr_count > 1)
+			return (0);
+		if (input[i] >= '1' && input[i] <= '4')
+		{
+			nbr_count++;
+			spc_count = 0;
+		}
+        else if (input[i] == ' ')
+		{
+			spc_count++;
+			nbr_count = 0;
+		} 
+		else
+			return (0);
+        i++;
 	}
 	if (i != 31)
-		return (1);
-	return (0);
+		return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	if (check_input_for_error(argc, argv))
+	if (!check_input(argc, argv[1]))
 	{
 		write(1, "Error\n", 6);
 		return (-1);
